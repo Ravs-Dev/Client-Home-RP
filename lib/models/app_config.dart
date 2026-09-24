a8dead0138e40ec2daf48f3aaecef18c598f922e
Ravs-Dev <ravszz0101@gmail.com>
2026-09-24 20:09:09 +0700

    Update Client Samp

--- lib/models/app_config.dart
+++ lib/models/app_config.dart
@@ -1,11 +1,15 @@
 class AppConfig {
-  // IP Server yang benar
+  // 1. Konfigurasi Server & Download (Properti Statis / Const)
   static const String serverIp = "165.101.18.1";
   static const int serverPort = 7001;
 
-  // Direct Download Link GitHub Release
   static const String urlDataSamp =
       "https://github.com/Ravs-Dev/Client-Home-RP/releases/download/v1.0.0/data_samp.zip";
   static const String urlDataGangster =
       "https://github.com/Ravs-Dev/Client-Home-RP/releases/download/v1.0.0/data_gangster.zip";
+
+  // 2. Konfigurasi Pengguna (Variabel Dinamis yang Dibutuhkan oleh Settings Screen)
+  static String nickname = "Player";
+  static bool fastConnect = false;
+  static bool fpsCounter = true;
 }
